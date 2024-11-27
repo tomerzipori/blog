eb23ff015c7f421b65436475c15bac4b8c671bf3
Tomer Zipori <tomerzip@post.bgu.ac.il>
2024-11-27 21:03:35 +0200

    new logo

--- static/hex_generation.R
+++ static/hex_generation.R
@@ -1,10 +1,11 @@
 library(hexSticker)
 library(tidyverse)
+library(ggpubr)
 library(bayestestR)
 library(tidybayes)
 library(ggdist)
 
-lyengar2022_b_ordinal_model_impersonation <- read_rds("C:/Users/tomer/OneDrive/GitHub/SDT_inoculation/models/lyengar2022_b_ordinal_model_impersonation.rds")
+lyengar2022_b_ordinal_model_impersonation <- read_rds("~/GitHub/SDT_inoculation/models/lyengar2022_bayes_ordinal_model_impersonation.rds")
 
 draws <- gather_draws(lyengar2022_b_ordinal_model_impersonation, b_Intercept[Response])
 
@@ -35,10 +36,15 @@ draws <- gather_draws(lyengar2022_b_ordinal_model_impersonation, b_Intercept[Res
                             Response == 5 ~ .value - 34,
                             Response == 6 ~ .value - 51))
 
+image <- png::readPNG("static/chill-guy.png")
+
+im2 <- matrix(rgb(image[,,1],image[,,2],image[,,3], image[,,4] * 0.5), nrow = dim(image)[1]) # alpha = 0.5
+
 p <- draws |>
   ggplot() +
+  background_image(im2) +
   stat_slab(aes(x = .value, fill = ordered(Response)),
-            color = "gray14", alpha = 1, key_glyph = "polygon", linewidth = 0.36) +
+            color = "gray14", alpha = 0.8, key_glyph = "polygon", linewidth = 0.36) +
   scale_fill_discrete(type = c("#f94144", "#f8961e", "#f9c74f", "#90be6d", "#43aa8b", "#577590")) +
   scale_y_continuous(limits = c(-1, 1)) +
   theme_classic() +
@@ -52,8 +58,8 @@ p <- draws |>
 
 p
 
-s <- sticker(p, package = "cool hex logo", p_color = "#1e1f21", p_size = 20, s_x=1, s_y=.57, s_width=2, s_height=1.8,
-             h_fill="#6e5774", h_color="black", filename = "static/favicon.png")
+s <- sticker(p, package = "chill hex logo", p_color = "#1e1f21", p_size = 20, s_x=1, s_y=.57, s_width=2, s_height=1.8,
+             h_fill="#6e5774", h_color="black", filename = "static/favicon.png", white_around_sticker = T)
 
 plot(s)
 
